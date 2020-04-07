c26e4814c7a8be75bb2f97e298e3b068f5a78d29
Ritaban Biswas <48176820+notanoobcoder@users.noreply.github.com>
2020-04-07 11:46:19 +0530

    Add files via upload

---
+++ COVID-19 .py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+#Analysis of number of confirmed cases of COVID19, as of 28th March, 2020.
+import sys; sys.path
+import numpy as np 
+import pandas as pd 
+import plotly as py
+import plotly.express as px
+import plotly.graph_objs as go
+from plotly.subplots import make_subplots
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+init_notebook_mode(connected=True)
+# Reading data from the csv file containing no. of cases, countrywise
+df = pd.read_csv('covid_19_data.csv')
+
+df = df.rename(columns={'Country/Region':'Country'})
+df = df.rename(columns={'ObservationDate':'Date'})
+#Manipulating Dataframe
+df_countries = df.groupby(['Country', 'Date']).sum().reset_index().sort_values('Date', ascending=False)
+df_countries = df_countries.drop_duplicates(subset = ['Country'])
+df_countries = df_countries[df_countries['Confirmed']>0]
+#Creating Choropleth 
+fig = go.Figure(data=go.Choropleth(
+    locations = df_countries['Country'],
+    locationmode = 'country names',
+    z = df_countries['Confirmed'],
+    colorscale = 'Reds',
+    marker_line_color = 'black',
+    marker_line_width = 0.6,
+))
+fig.update_layout(
+    title_text = 'Confirmed Cases, as of March 28, 2020:',
+    title_x = 0.1,
+    geo=dict(
+        showframe = False,
+        showcoastlines = False,
+        projection_type = 'equirectangular'
+    )
+)
+#End
+
+
+# In[6]:
+
+
+#Analysis of number of confirmed cases of COVID19, as of 3rd April, 2020.
+import sys; sys.path
+import numpy as np 
+import pandas as pd 
+import plotly as py
+import plotly.express as px
+import plotly.graph_objs as go
+from plotly.subplots import make_subplots
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+init_notebook_mode(connected=True)
+# Reading data from the csv file containing no. of cases, countrywise
+df = pd.read_csv('covid_19_data.csv')
+# Renaming columns
+df = df.rename(columns={'Country/Region':'Country'})
+df = df.rename(columns={'ObservationDate':'Date'})
+# Manipulating the original dataframe
+df_countrydate = df[df['Confirmed']>0]
+df_countrydate = df_countrydate.groupby(['Date','Country']).sum().reset_index()
+df_countrydate
+# Creating the visualization
+fig = px.choropleth(df_countrydate, 
+                    locations="Country", 
+                    locationmode = "country names",
+                    color="Confirmed", 
+                    hover_name="Country", 
+                    animation_frame="Date"
+                   )
+fig.update_layout(
+    title_text = '//Global Spread of COVID-19//',
+    title_x = 0.5,
+    geo=dict(
+        showframe = False,
+        showcoastlines = False,
+    ))
+    
+fig.show()
+#End
+
+
+# In[ ]:
+
+
+
+
